56e6693a2c3b4eb32f713828c1cf636f151cf542
Ryan Campbell <campbellryan19@gmail.com>
2020-10-11 07:28:16 -0400

    Creating classes for Count Min Sketch and Markov models

---
+++ CountMin.py
@@ -0,0 +1,16 @@
+
+"""
+Count Min Sketch for approximating counts of distinct elements.
+"""
+
+# TODO Get 2-universal hash functions
+
+class CountMin():
+    def __init__(self, num_hash, length_table):
+        pass
+
+    def update(self, elem):
+        pass
+
+    def get_count(self, elem):
+        pass
---
+++ Markov.py
@@ -0,0 +1,14 @@
+
+"""
+Markov Chain Text Generator
+"""
+
+class Markov():
+    # Handle taking text input -> hashable elements
+    pass
+
+class CountMinMarkov(Markov):
+    pass
+
+class ExactMarkov(Markov):
+    pass
